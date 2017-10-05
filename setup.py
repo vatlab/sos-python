348dc3dd0f7db8339209bd0714a91fb1d24c0d19
Bo Peng <ben.bog@gmail.com>
2017-10-05 00:51:13 -0500

    Add SoS Notebook extensions for Python2 and Python3, separated from sos

---
+++ setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#
+# This file is part of Script of Scripts (sos), a workflow system
+# for the execution of commands and scripts in different languages.
+# Please visit https://github.com/vatlab/SOS for more information.
+#
+# Copyright (C) 2016 Bo Peng (bpeng@mdanderson.org)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from setuptools import find_packages, setup
+
+# obtain version of SoS
+with open('src/sos_python/_version.py') as version:
+    for line in version:
+        if line.startswith('__version__'):
+            __version__ = eval(line.split('=')[1])
+            break
+
+setup(name = "sos-python",
+    version = __version__,
+    description = 'SoS Notebook extension for languages Python 2 and Python 2',
+    author = 'Bo Peng',
+    url = 'https://github.com/vatlab/SOS',
+    author_email = 'bpeng@mdanderson.org',
+    maintainer = 'Bo Peng',
+    maintainer_email = 'bpeng@mdanderson.org',
+    license = 'GPL3',
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        ],
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    install_requires=[
+          'sos',
+          'sos-notebook',
+      ],
+    entry_points= '''
+[sos_targets]
+Py_Module = sos_python.target:Py_Module
+
+[sos_actions]
+python = sos_python.actions:python
+python2 = sos_python.actions:python2
+python3 = sos_python.actions:python3
+
+[sos_languages]
+Python2 = sos_python.kernel:sos_Python
+Python3 = sos_python.kernel:sos_Python
+'''
+)
