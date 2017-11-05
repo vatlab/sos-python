32035d38e863c7e2413ad5aec18938d79a4a02a2
Bo Peng <ben.bog@gmail.com>
2017-11-05 13:49:18 -0600

    Fix python test

--- test/test_Python3_target.py
+++ test/test_Python3_target.py
@@ -45,7 +45,7 @@ depends: Py_Module('tabulate')
 from tabulate import tabulate
 table = [["Sun",696000,1989100000],["Earth",6371,5973.6],
     ["Moon",1737,73.5],["Mars",3390,641.85]]
-report: output='report.md'
+report: output='report.md', expand=True
     {tabulate(table)}
 ''')
         wf = script.workflow()
